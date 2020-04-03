e6525b145ae772b42dd5f9537ec9db2c5ae3bb5a
Duncan-Kiragu <mathenge089@gmail.com>
2020-04-03 12:46:22 +0300

    fixing spacing and indentation in name component files

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,23 +1,23 @@
-  import { Component, OnInit , Output, EventEmitter, Directive} from '@angular/core';
-  import { Quote } from '../quote'
+import { Component, OnInit , Output, EventEmitter, Directive} from '@angular/core';
+import { Quote } from '../quote'
 
-    @Component({
-        selector: 'app-quote-form',
-        templateUrl: './quote-form.component.html',
-        styleUrls: ['./quote-form.component.css']
-      })
-    
-    export class QuoteFormComponent implements OnInit {
-      newQuote= new Quote (0 ,0, "" ,"","", new Date());
-      @Output() addQuote = new EventEmitter <Quote>()
+  @Component({
+      selector: 'app-quote-form',
+      templateUrl: './quote-form.component.html',
+      styleUrls: ['./quote-form.component.css']
+    })
+  
+  export class QuoteFormComponent implements OnInit {
+    newQuote= new Quote (0 ,0, "" ,"","", new Date());
+    @Output() addQuote = new EventEmitter <Quote>()
 
-        submitQuote(){
-          this.addQuote.emit(this.newQuote);
-        }
-
-      constructor() { }
-
-      ngOnInit(){
+      submitQuote(){
+        this.addQuote.emit(this.newQuote);
       }
+
+    constructor() { }
+
+    ngOnInit(){
     }
+  }
 
